1c4d75ea9b9531a1ef3c718ff1cb70c3e5a9be45
Bkliger <bkliger@comcast.net>
2016-06-25 19:34:33 -0700

    style changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@
 $(function(){
     $('nav').localScroll({duration:150, hash: true});
     $('.navcaret').localScroll({duration:150, hash: true});
-    $('#YMTC').player.setPlaybackQuality('hd720'):Void;
+
 
 });
